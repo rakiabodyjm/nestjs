955c29b7ef0f171353393c08f48ec4fa9adbeb87
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2021-07-07 11:26:33 +0800

    Initialize Project

---
+++ src/validation/validation.pipe.ts
@@ -0,0 +1,31 @@
+import {
+  ArgumentMetadata,
+  BadRequestException,
+  PipeTransform,
+} from '@nestjs/common'
+import { plainToClass } from 'class-transformer'
+import { validate } from 'class-validator'
+
+export class ValidationPipe implements PipeTransform<any> {
+  async transform(value: any, { metatype }: ArgumentMetadata) {
+    if (!metatype || !this.toValidate(metatype)) {
+      return value
+    }
+    const object = plainToClass(metatype, value)
+    console.log('object', object)
+    const errors = await validate(object, {
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    })
+
+    if (errors.length > 0) {
+      throw new BadRequestException('Validation Failed')
+    }
+    return value
+  }
+
+  private toValidate(metatype: Function): boolean {
+    const types: Function[] = [String, Boolean, Number, Array, Object]
+    return !types.includes(metatype)
+  }
+}
